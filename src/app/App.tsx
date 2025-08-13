d9471bb05b412fada1f15496e2f9c798604c04b4
nemo-nemo-bim <drakequation@naver.com>
2025-08-13 22:16:10 +0900

    💡 feat(routes): 기본 경로에 PostsPage 라우터 설정
    - 루트('/') 경로를 PostsPage로 매핑
    - 앱 진입 시 기본 화면을 PostsPage로 렌더링

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,6 @@
-import PostsManagerPage from "@/features/posts/PostsManagerPage"
+import { Route, Routes } from "react-router-dom"
+
+import { PostsPage } from "@/pages"
 import { BaseLayout } from "@/widgets/layout"
 
 import { AppProvider } from "./providers"
@@ -7,7 +9,9 @@ const App = () => {
   return (
     <AppProvider>
       <BaseLayout>
-        <PostsManagerPage />
+        <Routes>
+          <Route element={<PostsPage />} path="/" />
+        </Routes>
       </BaseLayout>
     </AppProvider>
   )
---
+++ src/pages/PostsPage.tsx
@@ -0,0 +1,5 @@
+import PostsManagerPage from "@/features/posts/PostsManagerPage"
+
+export const PostsPage = () => {
+  return <PostsManagerPage />
+}
